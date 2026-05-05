94c4cfe1b6f03bc00a129571f77bb7de38e2d692
Kamil Figiela <kamil.figiela@soostone.com>
2026-05-05 20:32:22 +0000

    Improve Appender API

--- duckdb-simple/src/Database/DuckDB/Simple/Appender.hs
+++ duckdb-simple/src/Database/DuckDB/Simple/Appender.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 {- |
 Module      : Database.DuckDB.Simple.Appender
@@ -8,6 +10,7 @@ module Database.DuckDB.Simple.Appender
   ( withTableAppender
   , withTableAppenderExt
   , withQueryAppender
+  , appendTableRow
   )
   where
 
@@ -20,7 +23,7 @@ import Database.DuckDB.FFI
       c_duckdb_appender_create,
       c_duckdb_appender_create_ext,
       c_duckdb_appender_create_query,
-      c_duckdb_appender_destroy )
+      c_duckdb_appender_destroy, c_duckdb_appender_begin_row, c_duckdb_append_value, c_duckdb_appender_end_row, c_duckdb_appender_flush, c_duckdb_appender_close )
 import Data.Text.Foreign (withCString)
 import Data.Text (Text)
 import Foreign (Ptr, nullPtr, Storable (peek))
@@ -28,7 +31,9 @@ import Foreign.Marshal.Alloc (alloca)
 import Foreign.Marshal.Array (withArray)
 import Control.Exception (finally, throwIO)
 import Control.Monad (when)
-import Database.DuckDB.Simple.Internal (withConnectionHandle, Connection)
+import Database.DuckDB.Simple.Internal (withConnectionHandle, Connection, SQLError(..))
+import GHC.Stack (HasCallStack)
+import Database.DuckDB.Simple.Generic (ToTable(toTableRowValues))
 
 type TableName = Text
 
@@ -61,10 +66,30 @@ withAppenderAcquire acquire action =
         when (state  /= DuckDBSuccess) $ throwIO (userError "duckdb-simple: could not acquire appender")
         case state of
           DuckDBSuccess -> pure ()
-          DuckDBError -> error "withAppenderAcquire"
+          DuckDBError -> throwIO (userError "withAppenderAcquire")
         app <- peek appPtr
         let release = do
               destroyState <- c_duckdb_appender_destroy appPtr
-              pure ()
-            --   when (destroyState  /= DuckDBSuccess) $ throwIO (userError "duckdb-simple: could not release appender")
-        action app `finally` release
+              when (destroyState  /= DuckDBSuccess) $ throwIO (userError "duckdb-simple: could not release appender")
+            flushAndClose = do
+                -- errPtr0 <- c_duckdb_appender_error_data app
+                --       when (errPtr0 /= nullPtr) $ do
+                --                         msg <- peekCString errPtr0
+                --                         error $ toText msg
+                flushState <- c_duckdb_appender_flush app
+                when (flushState  /= DuckDBSuccess) $ throwIO (userError "duckdb-simple: could not release appender")
+                closeState <- c_duckdb_appender_close app
+                when (closeState  /= DuckDBSuccess) $ throwIO (userError "duckdb-simple: could not release appender")
+
+        (action app <* flushAndClose) `finally` release
+
+appendTableRow :: (HasCallStack, ToTable a) => DuckDBAppender -> a -> IO ()
+appendTableRow app row = do
+    assertSuccess $ c_duckdb_appender_begin_row app
+    toTableRowValues row >>= mapM_ (assertSuccess . c_duckdb_append_value app)
+    assertSuccess $ c_duckdb_appender_end_row app
+    where
+    assertSuccess :: (HasCallStack) => IO DuckDBState -> IO ()
+    assertSuccess f = f >>= \case
+      DuckDBSuccess -> pure ()
+      _errorStatus -> throwIO $ SQLError "duckdb-simple: appendTableRow status error" Nothing Nothing -- FIXME: proper error handling
